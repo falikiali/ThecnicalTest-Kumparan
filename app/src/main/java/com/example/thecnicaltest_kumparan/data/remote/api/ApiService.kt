632221dfc9972b01d4423055b214e7af67dbb3b6
MyCapstone <alifaliki456@gmail.com>
2021-10-18 18:16:47 +0700

    make ApiService.kt for get data from some endpoint in API

---
+++ app/src/main/java/com/example/thecnicaltest_kumparan/data/remote/api/ApiService.kt
@@ -0,0 +1,13 @@
+package com.example.thecnicaltest_kumparan.data.remote.api
+
+import com.example.thecnicaltest_kumparan.data.remote.responses.PostResponsesItem
+import com.example.thecnicaltest_kumparan.data.remote.responses.UserResponsesItem
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("posts")
+    suspend fun getPosts(): List<PostResponsesItem>
+
+    @GET("users")
+    suspend fun getUsers(): List<UserResponsesItem>
+}
