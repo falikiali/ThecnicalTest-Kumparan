38ce6e7798e589a886557dcc309987c412a09931
MyCapstone <alifaliki456@gmail.com>
2021-10-18 20:06:37 +0700

    adding parcelize

--- app/src/main/java/com/example/thecnicaltest_kumparan/data/Post.kt
+++ app/src/main/java/com/example/thecnicaltest_kumparan/data/Post.kt
@@ -1,10 +1,14 @@
 package com.example.thecnicaltest_kumparan.data
 
-data class Posts(
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Post(
     val id: Int,
     val userId: Int,
     val userName: String,
     val userCompany: String,
     val title: String,
     val body: String
-)
+) : Parcelable
