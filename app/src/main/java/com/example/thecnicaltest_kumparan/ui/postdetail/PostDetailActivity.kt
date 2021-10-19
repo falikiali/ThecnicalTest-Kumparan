ac679acc2b52256f80720b059117cd09be9b509c
MyCapstone <alifaliki456@gmail.com>
2021-10-19 22:33:31 +0700

    add new activity

---
+++ app/src/main/java/com/example/thecnicaltest_kumparan/ui/postdetail/PostDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thecnicaltest_kumparan.ui.postdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.thecnicaltest_kumparan.R
+
+class PostDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_detail)
+    }
+}
