26c2229e1bdaac1a2f061a424cc86051a5d4a7b6
MyCapstone <alifaliki456@gmail.com>
2021-10-20 22:03:12 +0700

    add new activity

---
+++ app/src/main/java/com/example/thecnicaltest_kumparan/ui/userdetail/UserDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thecnicaltest_kumparan.ui.userdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.thecnicaltest_kumparan.R
+
+class UserDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
